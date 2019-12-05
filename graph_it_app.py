396ac242e486449ac060ea2457f423d7ca06461f
Phillyclause89 <45711864+Phillyclause89@users.noreply.github.com>
2019-12-05 07:49:00 -0800

    make it better

--- graph_it_app.py
+++ graph_it_app.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     """
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', ]
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server="https://pandadasherdemo.herokuapp.com/")
+    server = app.server
     # >>> Example call of series_graph_it(...) func
     app.layout = html.Div(
         # ...
